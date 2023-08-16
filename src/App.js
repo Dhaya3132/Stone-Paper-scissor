036d7f4c78c0813dbef5663488bd97765223c845
Dhayalan <135351029+Dhaya3132@users.noreply.github.com>
2023-08-16 11:05:28 +0530

    Updated 5 App.js

--- src/App.js
+++ src/App.js
@@ -77,24 +77,24 @@ function paper()
   {
     document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN STONE ";
     document.getElementById("content1").innerHTML="YOU WON THE MATCH";
-      // Draw++;
-      // document.getElementById("Computer_scores2").innerHTML=Draw;
+    user++;
+    document.getElementById("content4").innerHTML=user;
   }
   else if(random===2)
   {
 
     document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN PAPER ";
     document.getElementById("content1").innerHTML="DRAW OF THE MATCH";
-      // count++;
-      // document.getElementById("Computer_scores1").innerHTML=count;
+    Draw++;
+    document.getElementById("content2").innerHTML=Draw;
   }
   else if(random===3)
   {
 
     document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN SCISSOR ";
     document.getElementById("content1").innerHTML="COMPUTER WON THE MATCH";
-      // user++;
-      // document.getElementById("Computer_scores0").innerHTML=user;
+    Computer++;
+    document.getElementById("content3").innerHTML=Computer;
   }
         
 }
@@ -108,280 +108,24 @@ function scissor()
   {
     document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN STONE ";
     document.getElementById("content1").innerHTML="COMPUTER WON THE MATCH";
-      // Draw++;
-      // document.getElementById("Computer_scores2").innerHTML=Draw;
+    Computer++;
+    document.getElementById("content3").innerHTML=Computer;
   }
   else if(random===2)
   {
 
     document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN PAPER ";
     document.getElementById("content1").innerHTML="YOU WON THE MATCH";
-      // count++;
-      // document.getElementById("Computer_scores1").innerHTML=count;
+    user++;
+    document.getElementById("content4").innerHTML=user;
   }
   else if(random===3)
   {
 
     document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN SCISSOR ";
     document.getElementById("content1").innerHTML="DRAW OF THE MATCH";
-      // user++;
-      // document.getElementById("Computer_scores0").innerHTML=user;
-  }
-
-}
-
-
-
-export default App;
-import './App.css';
-
-function App() {
-  return (
-
-    <div className="container">
-
-      <div className="inner">
-
-        <div className="clicking">
-
-          <p onClick={stoner}>STONE</p>
-          <p onClick={paper}>PAPER</p>
-          <p onClick={scissor}>SCISSOR</p>
-           
-        </div>
-
-        <div className="Winners">
-
-          <p id="content" ></p>
-          <p id="content1"></p>
-          <p id="content2"></p>
-
-        </div>
-
-      </div>
-
-    </div>
-  );
-}
-
-let Draw= 0;
-function stoner()
-{
-    
-
-    let random=Math.ceil(Math.random()*3);
-    if(random===1)
-    {
-      document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN STONE ";
-      document.getElementById("content1").innerHTML="DRAW OF THE MATCH";
-      Draw++;
-      document.getElementById("content2").innerHTML=Draw;
-    }
-    else if(random===2)
-    {
-
-      document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN PAPER ";
-      document.getElementById("content1").innerHTML="COMPUTER WON THE MATCH";
-      // count++;
-      // document.getElementById("Computer_scores1").innerHTML=count;
-    }
-    else if(random===3)
-    {
-
-      document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN SCISSOR ";
-      document.getElementById("content1").innerHTML="YOU WON THE MATCH";
-      // user++;
-      // document.getElementById("Computer_scores0").innerHTML=user;
-    }
-
-}
-
-// eslint-disable-next-line no-unused-vars
-function paper()
-{
-
-  let random=Math.ceil(Math.random()*3);
-  if(random===1)
-  {
-    document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN STONE ";
-    document.getElementById("content1").innerHTML="YOU WON THE MATCH";
-      // Draw++;
-      // document.getElementById("Computer_scores2").innerHTML=Draw;
-  }
-  else if(random===2)
-  {
-
-    document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN PAPER ";
-    document.getElementById("content1").innerHTML="DRAW OF THE MATCH";
-      // count++;
-      // document.getElementById("Computer_scores1").innerHTML=count;
-  }
-  else if(random===3)
-  {
-
-    document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN SCISSOR ";
-    document.getElementById("content1").innerHTML="COMPUTER WON THE MATCH";
-      // user++;
-      // document.getElementById("Computer_scores0").innerHTML=user;
-  }
-        
-}
-
-
-function scissor()
-{
-
-  let random=Math.ceil(Math.random()*3);
-  if(random===1)
-  {
-    document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN STONE ";
-    document.getElementById("content1").innerHTML="COMPUTER WON THE MATCH";
-      // Draw++;
-      // document.getElementById("Computer_scores2").innerHTML=Draw;
-  }
-  else if(random===2)
-  {
-
-    document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN PAPER ";
-    document.getElementById("content1").innerHTML="YOU WON THE MATCH";
-      // count++;
-      // document.getElementById("Computer_scores1").innerHTML=count;
-  }
-  else if(random===3)
-  {
-
-    document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN SCISSOR ";
-    document.getElementById("content1").innerHTML="DRAW OF THE MATCH";
-      // user++;
-      // document.getElementById("Computer_scores0").innerHTML=user;
-  }
-
-}
-
-
-
-export default App;
-import './App.css';
-
-function App() {
-  return (
-
-    <div className="container">
-
-      <div className="inner">
-
-        <div className="clicking">
-
-          <p onClick={stoner}>STONE</p>
-          <p onClick={paper}>PAPER</p>
-          <p onClick={scissor}>SCISSOR</p>
-           
-        </div>
-
-        <div className="Winners">
-
-          <p id="content" ></p>
-          <p id="content1"></p>
-
-        </div>
-
-      </div>
-
-    </div>
-  );
-}
-
-
-
-function stoner()
-{
-    // let Draw,count,user = 0;
-
-    let random=Math.ceil(Math.random()*3);
-    if(random===1)
-    {
-      document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN STONE ";
-      document.getElementById("content1").innerHTML="DRAW OF THE MATCH";
-      // Draw++;
-      // document.getElementById("Computer_scores2").innerHTML=Draw;
-    }
-    else if(random===2)
-    {
-
-      document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN PAPER ";
-      document.getElementById("content1").innerHTML="COMPUTER WON THE MATCH";
-      // count++;
-      // document.getElementById("Computer_scores1").innerHTML=count;
-    }
-    else if(random===3)
-    {
-
-      document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN SCISSOR ";
-      document.getElementById("content1").innerHTML="YOU WON THE MATCH";
-      // user++;
-      // document.getElementById("Computer_scores0").innerHTML=user;
-    }
-
-}
-
-// eslint-disable-next-line no-unused-vars
-function paper()
-{
-
-  let random=Math.ceil(Math.random()*3);
-  if(random===1)
-  {
-    document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN STONE ";
-    document.getElementById("content1").innerHTML="YOU WON THE MATCH";
-      // Draw++;
-      // document.getElementById("Computer_scores2").innerHTML=Draw;
-  }
-  else if(random===2)
-  {
-
-    document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN PAPER ";
-    document.getElementById("content1").innerHTML="DRAW OF THE MATCH";
-      // count++;
-      // document.getElementById("Computer_scores1").innerHTML=count;
-  }
-  else if(random===3)
-  {
-
-    document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN SCISSOR ";
-    document.getElementById("content1").innerHTML="COMPUTER WON THE MATCH";
-      // user++;
-      // document.getElementById("Computer_scores0").innerHTML=user;
-  }
-        
-}
-
-
-function scissor()
-{
-
-  let random=Math.ceil(Math.random()*3);
-  if(random===1)
-  {
-    document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN STONE ";
-    document.getElementById("content1").innerHTML="COMPUTER WON THE MATCH";
-      // Draw++;
-      // document.getElementById("Computer_scores2").innerHTML=Draw;
-  }
-  else if(random===2)
-  {
-
-    document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN PAPER ";
-    document.getElementById("content1").innerHTML="YOU WON THE MATCH";
-      // count++;
-      // document.getElementById("Computer_scores1").innerHTML=count;
-  }
-  else if(random===3)
-  {
-
-    document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN SCISSOR ";
-    document.getElementById("content1").innerHTML="DRAW OF THE MATCH";
-      // user++;
-      // document.getElementById("Computer_scores0").innerHTML=user;
+    Draw++;
+    document.getElementById("content2").innerHTML=Draw;
   }
 
 }
