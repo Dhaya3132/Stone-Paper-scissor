96641fd25b9e3e78b455b2255bfe2f7e2c93f5ec
Dhayalan <135351029+Dhaya3132@users.noreply.github.com>
2023-08-16 10:27:18 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -19,6 +19,134 @@ function App() {
 
           <p id="content" ></p>
           <p id="content1"></p>
+          <p id="content2"></p>
+
+        </div>
+
+      </div>
+
+    </div>
+  );
+}
+
+let Draw= 0;
+function stoner()
+{
+    
+
+    let random=Math.ceil(Math.random()*3);
+    if(random===1)
+    {
+      document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN STONE ";
+      document.getElementById("content1").innerHTML="DRAW OF THE MATCH";
+      Draw++;
+      document.getElementById("content2").innerHTML=Draw;
+    }
+    else if(random===2)
+    {
+
+      document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN PAPER ";
+      document.getElementById("content1").innerHTML="COMPUTER WON THE MATCH";
+      // count++;
+      // document.getElementById("Computer_scores1").innerHTML=count;
+    }
+    else if(random===3)
+    {
+
+      document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN SCISSOR ";
+      document.getElementById("content1").innerHTML="YOU WON THE MATCH";
+      // user++;
+      // document.getElementById("Computer_scores0").innerHTML=user;
+    }
+
+}
+
+// eslint-disable-next-line no-unused-vars
+function paper()
+{
+
+  let random=Math.ceil(Math.random()*3);
+  if(random===1)
+  {
+    document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN STONE ";
+    document.getElementById("content1").innerHTML="YOU WON THE MATCH";
+      // Draw++;
+      // document.getElementById("Computer_scores2").innerHTML=Draw;
+  }
+  else if(random===2)
+  {
+
+    document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN PAPER ";
+    document.getElementById("content1").innerHTML="DRAW OF THE MATCH";
+      // count++;
+      // document.getElementById("Computer_scores1").innerHTML=count;
+  }
+  else if(random===3)
+  {
+
+    document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN SCISSOR ";
+    document.getElementById("content1").innerHTML="COMPUTER WON THE MATCH";
+      // user++;
+      // document.getElementById("Computer_scores0").innerHTML=user;
+  }
+        
+}
+
+
+function scissor()
+{
+
+  let random=Math.ceil(Math.random()*3);
+  if(random===1)
+  {
+    document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN STONE ";
+    document.getElementById("content1").innerHTML="COMPUTER WON THE MATCH";
+      // Draw++;
+      // document.getElementById("Computer_scores2").innerHTML=Draw;
+  }
+  else if(random===2)
+  {
+
+    document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN PAPER ";
+    document.getElementById("content1").innerHTML="YOU WON THE MATCH";
+      // count++;
+      // document.getElementById("Computer_scores1").innerHTML=count;
+  }
+  else if(random===3)
+  {
+
+    document.getElementById("content").innerHTML="COMPUTER HAS CHOOSEN SCISSOR ";
+    document.getElementById("content1").innerHTML="DRAW OF THE MATCH";
+      // user++;
+      // document.getElementById("Computer_scores0").innerHTML=user;
+  }
+
+}
+
+
+
+export default App;
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="container">
+
+      <div className="inner">
+
+        <div className="clicking">
+
+          <p onClick={stoner}>STONE</p>
+          <p onClick={paper}>PAPER</p>
+          <p onClick={scissor}>SCISSOR</p>
+           
+        </div>
+
+        <div className="Winners">
+
+          <p id="content" ></p>
+          <p id="content1"></p>
 
         </div>
 
